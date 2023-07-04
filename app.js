97e217ca7f0cd0af9cc8422f8e2670e90e04a7b0
CyberIsidore <wizardisidore@gmail.com>
2023-07-04 11:53:43 -0400

    part one complete!
    part two in progress

---
+++ app.js
@@ -0,0 +1,26 @@
+// 14.4: rest and spread
+
+//
+// refactor function to use rest and arrow function
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function (num) {
+        return num % 2 === 0
+    });
+}
+
+const filterOutOdds2 = (...nums) => {
+    return nums.filter((num) => {
+        return num % 2 === 0
+    });
+}
+
+// Write a function called findMin that accepts a...
+// ...variable number of arguments and returns the smallest argument.
+// Make sure to do this using the rest and spread operator.
+
+const findMin = (...nums) => {
+    return nums.reduce((min, currVal) => {
+        return currVal < min ? currVal : min;
+    }, "nothing here.")
+}

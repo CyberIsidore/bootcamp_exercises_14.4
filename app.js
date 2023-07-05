5966e29b827c75365aa9f595641088e91b8dd429
CyberIsidore <wizardisidore@gmail.com>
2023-07-04 21:10:48 -0400

    done!

--- app.js
+++ app.js
@@ -37,3 +37,63 @@ const mergeObjects = ((a, b) => {
 
 const doubleAndReturnArgs = ((array, ...args) =>
     numbers = ([...array, ...args].map(x => x * 2)));
+
+// For this section, write the following functions using rest,
+// spread and refactor these functions to be arrow functions!
+
+// Make sure that you are always returning a new array or object and
+// not modifying the existing inputs.
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = ((...items) => {
+    while (items.length) {
+        const randomItem = Math.floor(Math.random() * items.length);
+        console.log(items[randomItem]);
+        items.splice(randomItem, 1);
+        return items;
+    }
+});
+
+/** Return a new array with every item in array1 and array2. */
+
+const extend = ((array1, array2) => both = [...array1, ...array2]);
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+// addKeyVal({
+//     type: 'oolong',
+//     name: 'winter sprout',
+//     origin: 'taiwan'
+// }, 'price', 22.99)
+
+const addKeyVal = ((obj, k, v) => {
+    const oldStuff = { ...obj };
+    const newStuff = { [k]: v };
+    return allStuff = { ...oldStuff, ...newStuff };
+});
+
+
+/** Return a new object with a key removed. */
+
+const removeKey = ((obj, k) => {
+    const newObj = { ...obj };
+    delete newObj[k]
+    return newObj;
+});
+
+
+/** Combine two objects and return a new object. */
+
+const combine = ((obj1, obj2) => {
+    return both = { ...obj1, ...obj2 }
+});
+
+
+/** Return a new object with a modified key and value. */
+
+const update = ((obj, key, val) => {
+    const Things = { ...obj };
+    return newThing = { ...Things, [key]: val };
+});
